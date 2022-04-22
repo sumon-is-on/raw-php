6d9694804fabc7599e07009a857d141e5d4c4d8c
sumon <18103159@iubat.edu>
2022-04-23 01:27:31 +0600

    hello

---
+++ Programme/hello.php
@@ -0,0 +1,10 @@
+<?php
+    # php program starts with  ' <?php ' and ends with   the lat two symbol of this code.
+    # here we will write a very simple code. this will show a output  "hello world!"
+
+    echo "hello world!";
+
+    # here echo is a keyword which will print the text "hello world". 
+    # we have printed some words which are strings. so wee need to write the strings in between the double quotation . and after this we have just used a semicolon . this semi-colon will end the statement. 
+    # so what is a statement? well every single line that ends with a semi-colon is a statement. we will learn about the ELEMENTS OF PHP in one of our doc file names "elements.php".
+?>
