a6592a31ca13b806a0d4c0809247fd0b11c38b58
sumon <18103159@iubat.edu>
2022-04-26 20:48:00 +0600

    statement

---
+++ Doc/statement.php
@@ -0,0 +1,10 @@
+<?php
+# every sentence that ends with a (semi-colon ;) is considered as a statement in php. for example,
+echo " hello world";
+
+# a statement can be written in a line , or in a multiple line . for example,
+echo " a quick brown fox jumps over a lazy dog. in this line every english alphabet is used in a sentence and this sebtence contains two line. but we will use only a semicolon to finish this statement . and it is a single statement";
+# in a single line we can have multiple statements . for example,
+echo "hello"; echo "world";
+
+?>
