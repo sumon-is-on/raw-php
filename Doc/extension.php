fbef1c89f927bff784bd23ad190e5801e9d706b0
sumon <18103159@iubat.edu>
2022-04-26 22:43:29 +0600

    php extensions update

--- Doc/extension.php
+++ Doc/extension.php
@@ -22,6 +22,8 @@
     
     # in every extension there are some built in function and method which does some seveal works. for example to work with the array extension we will use some of the built in function and method 
 
+
+            # example -1: ARRAY EXTENSION
             $cities=[
                         "DHAKA",
                         "CHITTAGONG",
@@ -40,6 +42,26 @@
             print_r($cities);
 
             # here we have used two of the array function called    count()  and   sort(). this two functions work different . sort() actually sorts the array accordingly and count() does count the array data. if we want to do the same , we need to write a lot of code but using this functions our works become easire and more readalbe . 
+
+
+
+            # example -2: STRING EXYENSION
+            $name="md sumon hossain";
+            echo "length :" . strlen($name) . "characters";
+            echo strtoupper($name);
+
+            # here we have used two of the array function called    strlen()  and   strtoupper(). this two functions work different . strlen() actually counts the string lenght and strtoupper() does the strings uppercase. if we want to do the same , we need to write a lot of code but using this functions our works become easire and more readalbe . 
+
+
+
+
+            # example -3: MATH EXTENSION
+            $angle=30;
+            echo sin($angle);
+            echo cos($angle);
+            echo tan($angle);
+
+            # these are the math extension and we do not need to use any type of calculation to fin out the value of sin , cos and tan . using the math extension fubtions we can easily get these values.if we want to do the same , we need to write a lot of code 
 
             # so we can use the extensions and the functions of those extensions to our work.
 
