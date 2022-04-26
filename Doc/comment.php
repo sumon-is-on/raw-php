569681034539ce5378afa33b557e52a029fc3f7a
sumon <18103159@iubat.edu>
2022-04-26 21:06:30 +0600

    comment

---
+++ Doc/comment.php
@@ -0,0 +1,13 @@
+<?php
+    # in php file we can use comment in two different way .
+
+    // number-1: single line comment, we have to use a doulbe slash (//) in  the beginning of a line 
+
+
+    /*
+    number-2: block comment
+    here, we need to use slash and star (/*) before and end of a line of 
+    */
+
+    # or we cal also we hash(#) in the beginning of a line ad cand use the line as a comment. exactly as same as we have used here
+?>
