87734df663d79552817f32c6e99955a16ee58277
sumon <18103159@iubat.edu>
2022-04-26 20:54:14 +0600

    php along with html

---
+++ Programme/htmlPhp.php
@@ -0,0 +1,9 @@
+<h2>User Information</h2>
+<?php
+    echo "Name: Sumon"
+?>
+<br>
+<?php
+    echo "Age:24";
+?>
+# we can write php code along with html code . for this reason we have to make sure we are using a php tag while we are using php instruction inside a php code.
