9b496a4d4f61b5a3db860be0db378bde2291f8c9
sumon <18103159@iubat.edu>
2022-05-24 13:50:44 +0600

    type casting

---
+++ Doc/chapter2/data/typeCasting.php
@@ -0,0 +1,14 @@
+<?php
+    # TYPE CASTING:
+        # when a type of data is transformed into another type of data , is called data TYPE CASTING.
+
+    # IMPORTANCE OF TYPE CASTING:
+        # type casting helps the memory to space allocation. it will make the program faster and smoother. lets have an example, 
+            #  suppose we have a list of age of 1000 people . for example ( mr. x   22.5 years  ) . now what will happen ? the processor will take it as a float datatype and will be needed 8 byte memory for each location. but if we want to show the output by type casting and cast into integer, it will take only 4 byte memory . so we can avoid unnecessary memory allocation.
+
+
+
+    # NOTE:
+        # in any function , if there is an integer type argument in any operator or control structure, we will get the integer value even if we pass the different types of data.
+
+        # we can not use comma (,) in any integer type data. it will not be processed.
