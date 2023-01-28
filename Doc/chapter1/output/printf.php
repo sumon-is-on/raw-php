2954fa267c4fba2cb7c5b1d4fe26e0d4e6fa809e
sumon <18103159@iubat.edu>
2023-01-29 00:01:00 +0600

    printf other activities and sprintf

--- Doc/chapter1/output/printf.php
+++ Doc/chapter1/output/printf.php
@@ -19,4 +19,46 @@ printf(" the octal value of %d is %o",0200000, 0200000);
 //  here , the %d specifier is expecting a decimal type value and %o is expecting an octal value.
 //  but when we pass the octal value the %d has converted the octal value in the decimal and shows the output.
 
+
+
+    # VARIABLE SWAPING IN PRINTF()
+    $n=5;
+    $m=4;
+    printf('number swaping %2d %1d',$n,$m);
+    echo "\n";
+    // for swaping the variable we need to place the specipiers by number and we can not use double quote .
+    // we need to use it in the single quote.
+
+    // we can swap the variable by this
+    printf('the equvalent of %1$d is %1$b',12);
+    echo "\n";
+    // if we want to print the value of any decimal number to any decimal digit
+
+    $n= 4.333333;
+    printf('%.2f',$n); // here we wanted 2 digit after the decimal. 
+    echo "\n";
+
+
+    // if we wnat to print any number in  specific format 
+    $n=44;
+    printf("%08d",$n);
+    echo "\n";
+
+    // if we ant to print any number after some digit of decilam and also using some format 
+    $n=4.333;
+    printf("%06.2f",$n);
+    echo "\n";
+
+    // we can use any of php function with printf
+
+    $myName="sumon";
+    printf("My name is %s",strtoupper($myName));
+    echo "\n";
+
+    // if we wnat to return anything then we need to use sprintf
+    $output=sprintf("My name is %s",$myName);
+    echo $output; // here sprintf functions returns the value and then stores in the output variable.
+                // tht means it returns the value in the output variable.
+    echo "\n";
+    echo strtoupper($output); // we can use any other functions to with the output.
 ?>
