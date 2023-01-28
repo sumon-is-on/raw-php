a0bc11427e945f1728308b513116a846561ccc36
sumon <18103159@iubat.edu>
2023-01-28 22:33:42 +0600

    var_dump output

---
+++ Doc/chapter1/output/vardump.php
@@ -0,0 +1,11 @@
+<?php
+    // if we want to know about the data type of a variable we can use the var_dump function of php.
+    // it will show us some information about the variable.
+    $number=100;
+    $myName="sumon";
+    $anotherNumber=33.33;
+    $trueFalse=true;
+    $hello=null;
+    var_dump($number,$myName,$anotherNumber,$trueFalse,$hello);
+    // here we can see the data type and the length of the varable by the var_dump function 
+?>
