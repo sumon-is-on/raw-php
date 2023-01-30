2e091d4d1a71e85690b374c3ea2b79d81d3bb5a4
sumon <18103159@iubat.edu>
2023-01-30 22:05:18 +0600

    switch case nested

--- Programme/logicalOperation.php/swicthCase.php
+++ Programme/logicalOperation.php/swicthCase.php
@@ -22,3 +22,48 @@ switch($color){
     default:
         printf("%s is not the rainbow color \n",$color);
 }
+
+
+// Now we will look through the nested switch case
+// lets think about a number which is even or odd and positive or negative
+
+$number=-13;
+switch($number){
+    case ($number%2==0):
+        switch($number){
+            case($number>0):
+                printf("%d is a positive even number\n",$number);
+                break;
+            case($number<0):
+                printf("%d is a negative even number\n",$number);
+                break;
+        }
+    break;
+        default:
+            switch($number){
+                case($number>0):
+                    printf("%d is a positive odd number\n",$number);
+                    break;
+                case($number<0):
+                    printf("%d is a negative odd number\n",$number);
+                    break;
+            }
+}
+
+// the above code is bit more complex to understand. we can make it more readable by the following code.
+// $number=-13;
+switch(true){
+    case ($number%2==0 && $number>0):
+        printf("%d is a positive even number\n",$number);
+        break;
+    case ($number%2==0 && $number<0):
+        printf("%d is a Negative even number\n",$number);
+        break;
+    case ($number%2==1 && $number>0):
+        printf("%d is a positive odd number\n",$number);
+        break;
+    case ($number%2==-1 && $number<0):
+        printf("%d is a negative even number\n",$number);
+        break;
+    
+}
