1e14f2f9354c0c2fc0cf38764ccc3ab49f3f4341
sumon <18103159@iubat.edu>
2023-02-03 00:32:32 +0600

    global variable value change

--- Programme/hello.php
+++ Programme/hello.php
@@ -7,4 +7,16 @@
     # here echo is a keyword which will print the text "hello world". 
     # we have printed some words which are strings. so wee need to write the strings in between the double quotation . and after this we have just used a semicolon . this semi-colon will end the statement. 
     # so what is a statement? well every single line that ends with a semi-colon is a statement. we will learn about the ELEMENTS OF PHP in one of our doc file names "elements.php".
+
+
+    # this is how we can change the value of global variable usig global keyword
+    $x=10;
+    printx();
+    echo $x;
+    echo PHP_EOL;
+    function printx(){
+        global $x;
+        $x=20;
+        echo $x;
+    }
 ?>
