783ecffa3a7790e592690cc192172722dc1656eb
sumon <18103159@iubat.edu>
2023-02-03 00:43:43 +0600

    php loop

---
+++ Programme/loop/phpLoop.php
@@ -0,0 +1,11 @@
+<?php
+/* in every programming language loop is a very common programming structure.
+loop basically reapeats the statements untill the defined conditions are true.
+
+here are some common loops in php
+    for 
+    foreach
+    while
+    do while
+    goto
+*/
