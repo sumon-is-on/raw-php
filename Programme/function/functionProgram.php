43e0de7a01c9010168b936b465b17c4bd52dd5d0
sumon <18103159@iubat.edu>
2023-02-07 00:02:34 +0600

    function unlimited parameter

--- Programme/function/functionProgram.php
+++ Programme/function/functionProgram.php
@@ -11,3 +11,28 @@ echo defaultParamete();
 $food="Beer";
 $quantity=3;
 echo defaultParamete($food,$quantity);
+
+
+# unlimited function
+function sum(int ...$numbers): int { // here after the parameter we have used a return type so that if we return different types of data, there will be a fatal error
+    $result=0;
+    for($i=0; $i<count($numbers);$i++){
+        $result+=$numbers[$i];
+    }
+    return $result;
+}
+
+echo sum(1,2,3,4);
+echo PHP_EOL;
+
+// we can use any type of parameter before the unlimited parameter but can not use after the unlimited parameter
+function sumOn(int $x, int $y, int ...$numbers): int { // here after the parameter we have used a return type so that if we return different types of data, there will be a fatal error
+    $result=0;
+    for($i=0; $i<count($numbers);$i++){
+        $result+=$numbers[$i];
+    }
+    return $result+$x+$y;
+}
+$number1=30;
+$number2=40;
+echo sum($number1,$number2,1,2,3,4);
