46f114909430f78d55c596df75a7b146d3c682f9
sumon <18103159@iubat.edu>
2023-02-06 22:03:11 +0600

    do while loop

---
+++ Programme/loop/doWhile.php
@@ -0,0 +1,11 @@
+<?php
+# Do while
+/* first the statement will be executed once weather the condition is true or not 
+then it will go for the check.
+*/
+$n=3;
+do{
+    printf("%d\n",$n);
+    $n--;
+}while($n>=5);
+# the do while loop must execute the statements once. but after checking the condition it will look for true or false.
