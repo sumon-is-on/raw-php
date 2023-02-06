f0f63ef96e39b9abf85086fa19d60473b54fde3f
sumon <18103159@iubat.edu>
2023-02-06 22:14:13 +0600

    foreach loop

---
+++ Programme/loop/foreach.php
@@ -0,0 +1,19 @@
+<?php
+# foreach loop 
+    # foreach loop basically works with array . it will take the array collection and frint as a single item.
+
+    // $name=[
+    //     $user=[
+    //         'name'=>"sumon",
+    //         'age'=>25,
+    //     ],
+    //     $user1=[
+    //         'name'=>"Selu",
+    //         'age'=>21
+    //     ]
+    // ];
+    // foreach($name as $data){
+    //     echo $name;
+    // }
+
+    # we will work with array later.
