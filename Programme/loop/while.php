a008ef84ebc32be004174da953ab98a7b02976f0
sumon <18103159@iubat.edu>
2023-02-06 21:57:15 +0600

    while loop

---
+++ Programme/loop/while.php
@@ -0,0 +1,24 @@
+<?php
+
+# While Loop
+
+/* while lop works untill or unless the condition is true. 
+first it will check the condition , if it is true , it will execute the statements and then agin 
+check the condition. now if it is true again it will execute the statements again, and got fo the condition . 
+this is how the while loop works.
+*/
+
+/* 
+while(expression){
+    statements;
+}
+*/
+# example:
+$n=5;
+while($n<=10){
+    printf("%d\n",$n);
+    // echo "hi";
+    $n++;
+}
+
+
