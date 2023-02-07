e1ac319edf767c40b38653a9a3a3206670c1f477
sumon <18103159@iubat.edu>
2023-02-07 22:25:03 +0600

    recurssive function factorial

---
+++ Programme/function/recursiveFunction/factorial.php
@@ -0,0 +1,20 @@
+<?php 
+# here we will print factorial of a number by recursive function.
+
+function factorial( int $n){
+    if($n<=1){
+        return 1;
+    }
+    return $n * factorial($n-1);
+    /* code explanation
+        declear n=5
+        steps;
+        n * factorial(n-1)
+        5 * factorial(5-1=4)  // 5*4
+        n * factorial(n-1)
+        4 * factorial(4-1=3) // 5*4*3
+        n * factorial(n-1) //5*4*3*2
+        n * factorial(n-1)//5*4*3*2*1
+
+}
+echo factorial(5);
