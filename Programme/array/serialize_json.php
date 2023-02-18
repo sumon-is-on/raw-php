b1cc5ed9b814bc49a686902446cf46ebef9646f2
sumon <18103159@iubat.edu>
2023-02-19 00:26:38 +0600

    associative array to string, serialized, json encoding

---
+++ Programme/array/serialize_json.php
@@ -0,0 +1,25 @@
+<?php
+# Associative array to string , 
+$student=[
+    'name'=>'sumon',
+    'age'=>24,
+    'department'=>'bcse',
+    'id'=>18103159
+];
+
+// now if we want to convert this array to string . for this we can use two way 
+    // - serialize the array 
+    // - jason encoding
+
+    # serializing array
+
+    $serializing = serialize($student);
+    echo $serializing; # array has been serialized. now if we want to use this and convert into array again ,
+    $serializing=unserialize($serializing);
+    print_r($serializing);
+
+    # json encoding
+    $encoding= json_encode($student);
+    echo $encoding; # it has converted to an object , if we want to make it an associative array again 
+    $encoding = json_decode($encoding,true);
+    print_r($encoding);
