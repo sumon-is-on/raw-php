2f7ece2126aa3f6011289415dd999ca4d5b5bdaa
sumon <18103159@iubat.edu>
2023-02-19 23:40:30 +0600

    array function

---
+++ Programme/array/array_function.php
@@ -0,0 +1,32 @@
+<?php
+#   array()     []
+    # this is how we can define a variable an arry
+#   array_shift()
+    # extracts the first element of an array
+#   array_unshift()
+    # insert an element in the first of an array
+#   array_push()
+    # insert a data in the last of an array
+#   array_pop()
+    # extracts the last element of an array
+
+#   explode()
+    # converts string to array
+#   join()
+    # converts array to string
+#   preg_split()
+    # converts string to array using multiple delimeter
+
+#   serialize()
+    # serialize an array
+#   unserialize()
+    # converts a serialized data to array
+#   json_encode()
+    # encode an array to json data object
+#   json_decode()
+    # decode a json object to array
+
+#   array_slice()
+    # extracting some elements without modifiying the main array
+#   aray_splice()
+    #extracting some elements by modifiying the main array and make the main array new. we can set a new data by array_splice() 
